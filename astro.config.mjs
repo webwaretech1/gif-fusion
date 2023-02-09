5ca52a3531334724ca347062a275c0da5ef2d48f
John Fontaine <john@webware.tech>
2023-02-09 21:59:35 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/edge-functions';
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  adapter: netlify(),
+  output: 'server'
+});
